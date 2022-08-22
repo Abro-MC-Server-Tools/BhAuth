7f231bc19fc1beed39bfcd650ad0dc924088db67
Bronuh <bronyx@yandex.ru>
2022-08-22 15:36:34 +0300

    Dont kill pls

--- src/main/java/ru/bronuh/bhauth/AuthState.java
+++
@@ -1,9 +0,0 @@
-package ru.bronuh.bhauth;
-
-public class AuthState {
-	public String name;
-	public long lastSeen;
-	public String lastIp;
-	public boolean isLoggedIn = false;
-	public int authAttempts = 0;
-}
